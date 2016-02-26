494a5c6f09643dcdd203d66c39ee9a7219ff1ca1
Brad London <london.brad@gmail.com>
2016-02-26 10:49:25 -0800

    allow user to input stylist into database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,8 +8,34 @@ import java.util.*;
 
 public class App {
 
-    public static void main(String[] args) {
-      staticFileLocation("/public");
-      String layout = "templates/layout.vtl";
-    }
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.all());
+      model.put("clients", Client.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylists", (request, response) -> {
+      HashMap<String, Object>model= new HashMap<String, Object>();
+      model.put("stylists", Stylist.all());
+      model.put("clients", Client.all());
+      model.put("template", "templates/stylists.vtl");
+      return new ModelAndView(model,layout);
+    }, new VelocityTemplateEngine());
+
+    post("/cuisines", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String stylistName = request.queryParams("stylistName");
+      Stylist newStylist = new Stylist (stylistName);
+      newStylist.save();
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/stylists.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
 }
