f439bb29df3b7313222e43ce36b50e941e3ace53
Brad London <london.brad@gmail.com>
2016-02-26 07:31:14 -0800

    setup all files

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,18 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+   }
+
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteStylistQuery = "DELETE FROM stylists *;";
+      String deleteClientQuery = "DELETE FROM clients *;";
+      con.createQuery(deleteStylistQuery).executeUpdate();
+      con.createQuery(deleteClientQuery).executeUpdate();
+    }
+  }
+}
